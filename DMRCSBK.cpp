79220290a6d8512432b09984183d68027b091b95
adrian <adrian@qradiolink.org>
2023-12-11 09:14:44 +0200

    Update copyrights

--- DMRCSBK.cpp
+++ DMRCSBK.cpp
@@ -1,6 +1,7 @@
 /*
  *   Copyright (C) 2015,2016,2020,2021,2022 by Jonathan Naylor G4KLX
  *   Copyright (C) 2019 by Patrick Maier DK5MP
+ *   Copyright (C) 2023 by Adrian Musceac YO8RZZ
  *
  *   This program is free software; you can redistribute it and/or modify
  *   it under the terms of the GNU General Public License as published by
